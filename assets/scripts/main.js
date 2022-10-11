d021c5323f3bfd410fae9320a3f5ea046392c1b9
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-10-11 19:33:55 +0100

    chore: render all errors into the review form

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -113,6 +113,14 @@ Vue.component('product', {
 Vue.component('product-review', {
   template: `
     <form class="review-form" @submit.prevent="sendForm">
+
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+
       <p>
         <label for="name">Name:</label>
         <input id="name" v-model="name">
